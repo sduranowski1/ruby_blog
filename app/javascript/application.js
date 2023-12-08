7ce7468984ffb9ca20c126cfd84f966601994c32
sduranowski1 <sduranowski1@gmail.com>
2023-12-08 18:52:26 +0100

    init

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,8 @@
 // Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
 import "@hotwired/turbo-rails"
 import "controllers"
+
+//= require_tree appear.min
+//= require_tree bootstrap.min
+//= require_tree circle-progress.min
+//= require_tree imagesloaded.pkgd.min
