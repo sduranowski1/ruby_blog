d5ee4940e00925abb5306d8507a77f26583a4da0
Szymon Duranowski <sduranowski1@gmail.com>
2023-12-10 15:11:06 +0100

    init

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,6 @@ module RubyBlog
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.autoloader = :classic
   end
 end
